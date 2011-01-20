064d2d263e2640e000c3ea133e2ef704b4751d0e
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-01-20 17:34:01 +0100

    Modify the recent version of the parseconfig class.

--- spec/parseconfig_spec.rb
+++ spec/parseconfig_spec.rb
@@ -4,10 +4,10 @@ describe 'ParseConfig' do
 
   describe 'being initialized' do
 
-    it 'should require config file variable' do
+    it "should not require a file variable" do
       lambda do
         @config = ParseConfig.new()
-      end.should raise_error(ArgumentError)
+      end.should_not raise_error(Errno::EACCES)
     end
 
     it 'should require a readable config file' do
@@ -25,21 +25,21 @@ describe 'ParseConfig' do
     end
 
     it 'should map the config keys to a hash' do
-      @config['key1'].should == 'val1'
-      @config['key2'].should == 'val2'
+      @config.params['key1'].should == 'val1'
+      @config.params['key2'].should == 'val2'
     end
 
     it 'should handle values with spaces' do
-      @config['key3'].should == 'this is a value with spaces, and it may work!'
+      @config.params['key3'].should == 'this is a value with spaces, and it may work!'
     end
 
     it 'should handle quoted values' do
-      @config['single_quotes'].should == '5555 North South 77th street'
-      @config['double_quotes'].should == "I ain't no fool"
+      @config.params['single_quotes'].should == '5555 North South 77th street'
+      @config.params['double_quotes'].should == "I ain't no fool"
     end
 
     it 'should handle keys with spaces' do
-      @config['key with spaces'].should == 'wahoo'
+      @config.params['key with spaces'].should == 'wahoo'
     end
 
   end
@@ -51,23 +51,23 @@ describe 'ParseConfig' do
     end
 
     it 'should handle top level keys just fine' do
-      @config['key1'].should == 'val1'
-      @config['key2'].should == 'val2'
+      @config.params['key1'].should == 'val1'
+      @config.params['key2'].should == 'val2'
     end
 
     it 'should create a nested hash for config file sections' do
-      @config['section1'].should_not be_nil
-      @config['section2'].should_not be_nil
+      @config.params['section1'].should_not be_nil
+      @config.params['section2'].should_not be_nil
     end
 
     it 'should set the values specified in those sections' do
-      @config['section1']['sec1_key1'].should == 'sec1_val1'
-      @config['section1']['sec1_key2'].should == 'sec1_val2'
+      @config.params['section1']['sec1_key1'].should == 'sec1_val1'
+      @config.params['section1']['sec1_key2'].should == 'sec1_val2'
     end
 
     it 'should handle duplicate keys' do
-      @config['section2']['key1'].should == 'val1'
-      @config['section2']['key2'].should == 'val2'
+      @config.params['section2']['key1'].should == 'val1'
+      @config.params['section2']['key2'].should == 'val2'
     end
 
   end
@@ -78,23 +78,23 @@ describe 'ParseConfig' do
     end
 
     it 'should create a nested hash for config file sections' do
-      @config['section1'].should_not be_nil
-      @config['section2'].should_not be_nil
+      @config.params['section1'].should_not be_nil
+      @config.params['section2'].should_not be_nil
     end
 
     it 'should set the values specified in those sections' do
-      @config['section1']['sec1_key1'].should == 'sec1_val1'
-      @config['section1']['sec1_key2'].should == 'sec1_val2'
+      @config.params['section1']['sec1_key1'].should == 'sec1_val1'
+      @config.params['section1']['sec1_key2'].should == 'sec1_val2'
     end
 
     it 'should handle duplicate keys' do
-      @config['section2']['key1'].should == 'val1'
-      @config['section2']['key2'].should == 'val2'
+      @config.params['section2']['key1'].should == 'val1'
+      @config.params['section2']['key2'].should == 'val2'
     end
 
     it 'should support inheritence' do
-      @config['section2']['sec1_key1'].should == 'sec1_val1'
-      @config['section2']['sec1_key2'].should == 'sec1_val2'
+      @config.params['section2']['sec1_key1'].should == 'sec1_val1'
+      @config.params['section2']['sec1_key2'].should == 'sec1_val2'
     end
   end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'parseconfig'
 
+include TechnoGate
+
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
